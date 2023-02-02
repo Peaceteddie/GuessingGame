c722dac37b09f2bd1e3dbb7a3c32650e79d6b85b
tithxa <tithxa@syntronic.com>
2023-02-02 14:40:11 +0100

    Implemented indexeddb control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,38 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { GamesComponent } from './games/games.component';
 import { EditorComponent } from './editor/editor.component';
+import { DBConfig, NgxIndexedDBModule } from 'ngx-indexed-db';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const dbConfig: DBConfig = {
+  name: 'library',
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: 'cards',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'url', keypath: 'url', options: { unique: false } },
+      ],
+    },
+  ],
+};
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, GamesComponent, EditorComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    GamesComponent,
+    EditorComponent,
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgxIndexedDBModule.forRoot(dbConfig),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { NgxIndexedDBModule, NgxIndexedDBService } from 'ngx-indexed-db';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss']
+  styleUrls: ['./editor.component.scss'],
 })
-export class EditorComponent {
+export class EditorComponent implements OnInit {
+  constructor(
+    private dbService: NgxIndexedDBService,
+    private formBuilder: FormBuilder
+  ) {}
 
+  addForm = this.formBuilder.group({
+    name: '',
+    url: '',
+  });
+
+  Items: any[] = [];
+
+  ngOnInit(): void {
+    this.GetAll();
+  }
+
+  GetAll() {
+    this.dbService.getAll('cards').subscribe((list) => (this.Items = list));
+  }
+
+  Delete(id: number) {
+    this.dbService.deleteByKey('cards', id).subscribe(() => this.GetAll());
+  }
+
+  onSubmit() {
+    this.dbService
+      .add('cards', {
+        name: this.addForm.value['name'],
+        url: this.addForm.value['url'],
+      })
+      .subscribe(() => this.GetAll());
+    this.addForm.reset();
+  }
 }
